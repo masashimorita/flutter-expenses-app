007477b9d3bcb1350d0e81f2641f746063a10afa
masashimorita <masashi.morita.mm@gmail.com>
2021-09-12 14:55:17 +0900

    refactor: 💡 Make New Transaction Form scrollable

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -23,11 +23,15 @@ class _NewTransactionState extends State<NewTransaction> {
     final enteredTitle = this._titleController.text;
     final enteredAmount = double.parse(this._amountController.text);
 
-    if (enteredTitle.isEmpty || enteredAmount <= 0 || this._selectedDate == null) {
+    if (enteredTitle.isEmpty ||
+        enteredAmount <= 0 ||
+        this._selectedDate == null) {
       return;
     }
 
-    this.widget.onPressedButton(enteredTitle, enteredAmount, this._selectedDate);
+    this
+        .widget
+        .onPressedButton(enteredTitle, enteredAmount, this._selectedDate);
     Navigator.of(context).pop();
   }
 
@@ -38,7 +42,7 @@ class _NewTransactionState extends State<NewTransaction> {
       firstDate: DateTime(2019, 1, 1),
       lastDate: DateTime.now(),
     ).then((pickedDate) {
-      if(pickedDate == null) {
+      if (pickedDate == null) {
         return;
       }
       setState(() {
@@ -48,55 +52,61 @@ class _NewTransactionState extends State<NewTransaction> {
   }
 
   String _generateDateString() {
-    if(this._selectedDate == null) {
+    if (this._selectedDate == null) {
       return 'No Date Chosen!';
     } else {
       return 'Picked Date: ${DateFormat.yMd().format(this._selectedDate!)}';
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: this._titleController,
-              onSubmitted: (_) => this._submitData(),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: this._amountController,
-              onSubmitted: (_) => this._submitData(),
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(this._generateDateString()),
-                  ),
-                  TextButton(
-                    child: Text(
-                      'Choose Date',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: this._presentDatePicker,
-                  )
-                ],
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: this._titleController,
+                onSubmitted: (_) => this._submitData(),
               ),
-            ),
-            ElevatedButton(
-              child: Text('Add Transaction'),
-              onPressed: this._submitData,
-            ),
-          ],
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: this._amountController,
+                onSubmitted: (_) => this._submitData(),
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(this._generateDateString()),
+                    ),
+                    TextButton(
+                      child: Text(
+                        'Choose Date',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: this._presentDatePicker,
+                    )
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                child: Text('Add Transaction'),
+                onPressed: this._submitData,
+              ),
+            ],
+          ),
         ),
       ),
     );
